12de7771af525b615be17cf5bd8616c824c21f51
AbrorilHuda <moh.abrorilhuda@gmail.com>
2025-08-25 20:16:48 +0700

    update data and optimasi

--- src/utils/image.js
+++ src/utils/image.js
@@ -23,4 +23,96 @@ export const images = [
     id: 6,
     url: "/kenangan/6.webp",
   },
+  {
+    id: 7,
+    url: "/kenangan/7.webp",
+  },
+  {
+    id: 8,
+    url: "/kenangan/8.webp",
+  },
+  {
+    id: 9,
+    url: "/kenangan/9.webp",
+  },
+  {
+    id: 10,
+    url: "/kenangan/10.webp",
+  },
+  {
+    id: 11,
+    url: "/kenangan/11.webp",
+  },
+  {
+    id: 12,
+    url: "/kenangan/12.webp",
+  },
+  {
+    id: 13,
+    url: "/kenangan/13.webp",
+  },
+  {
+    id: 14,
+    url: "/kenangan/14.webp",
+  },
+  {
+    id: 15,
+    url: "/kenangan/15.webp",
+  },
+  {
+    id: 16,
+    url: "/kenangan/16.webp",
+  },
+  {
+    id: 17,
+    url: "/kenangan/17.webp",
+  },
+  {
+    id: 18,
+    url: "/kenangan/18.webp",
+  },
+  {
+    id: 19,
+    url: "/kenangan/19.webp",
+  },
+  {
+    id: 20,
+    url: "/kenangan/20.webp",
+  },
+  {
+    id: 21,
+    url: "/kenangan/21.webp",
+  },
+  {
+    id: 22,
+    url: "/kenangan/22.webp",
+  },
+  {
+    id: 23,
+    url: "/kenangan/23.webp",
+  },
+  {
+    id: 24,
+    url: "/kenangan/24.webp",
+  },
+  {
+    id: 25,
+    url: "/kenangan/25.webp",
+  },
+  {
+    id: 26,
+    url: "/kenangan/26.webp",
+  },
+  {
+    id: 27,
+    url: "/kenangan/27.webp",
+  },
+  {
+    id: 28,
+    url: "/kenangan/28.webp",
+  },
+  {
+    id: 29,
+    url: "/kenangan/29.webp",
+  },
 ];
